28394394fd729dd6db7d293c5632426994a5ee79
Luca <59053718+luca-gg@users.noreply.github.com>
2020-11-16 12:47:34 +0100

    Create RegionEnterEvent.java

---
+++ RegionEnterEvent.java
@@ -0,0 +1,42 @@
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class RegionEnterEvent extends Event implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
+    private boolean cancelled = false;
+    private final Player player;
+    private final Region region;
+
+    public RegionEnterEvent(Player player, Region region) {
+        this.player = player;
+        this.region = region;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Region getRegion() {
+        return region;
+    }
+
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+}
